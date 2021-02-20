e8f670716d72ae0c15e009ef31cef69f1bfaa705
chr-ge <ace26@protonmail.com>
2021-02-20 12:38:15 -0500

    feat: design home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import styles from './styles'
+
+const image = require('../../../assets/images/Saly-1.png')
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <Text style={styles.heading}>Welcome to CoinFlip</Text>
+      <Text style={styles.subheading}>Invest your virtual $100,000 and compete with others.</Text>
     </View>
   )
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({})
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  image: {
+   height: '40%',
+   aspectRatio: 1
+  },
+  heading: {
+   fontSize: 30,
+   color: 'white',
+   fontWeight: 'bold',
+   marginTop: 50,
+   marginBottom: 20
+  },
+  subheading: {
+    fontSize: 20,
+    color: '#909090',
+    textAlign: 'center'
+  }
+})
+
+export default styles
