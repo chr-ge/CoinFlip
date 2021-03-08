d57c97be81d7d962fcd0382ead56e1869ddddf7d
chr-ge <ace26@protonmail.com>
2021-03-07 20:54:14 -0500

    feat(profile): fetch user data

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react'
-import { Image, Text, Pressable, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Image, Text, Pressable, View, ActivityIndicator } from 'react-native'
 import { Auth } from 'aws-amplify'
+import { API, graphqlOperation } from 'aws-amplify'
+import { getUser } from '../../graphql/queries'
 import { CommonActions, useNavigation } from '@react-navigation/native'
 import styles from './styles'
+import { useAppContext } from '../../contexts/AppContext'
 const image = require('../../../assets/images/Saly-16.png')
+
+type User = {
+  name: string
+  image: string
+  email: string
+  networth: number
+}
 
 const ProfileScreen = () => {
   const navigation = useNavigation()
-  const [user, setUser] = useState({
-    id: '1',
-    name: 'George',
-    email: 'george@example.com',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png',
-    netWorth: 100000,
-  })
+  const [user, setUser] = useState<User>()
+  const { userId } = useAppContext()
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res: any = await API.graphql(graphqlOperation(getUser, { id: userId }))
+        setUser(res.data.getUser)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchUser()
+  }, [])
 
   const signOut = async () => {
     await Auth.signOut()
@@ -24,6 +41,8 @@ const ProfileScreen = () => {
       })
     )
   }
+
+  if (!user) return <ActivityIndicator />
 
   return (
     <View style={styles.container}>
@@ -36,7 +55,7 @@ const ProfileScreen = () => {
             <Text style={styles.email}>{user.email}</Text>
           </View>
         </View>
-        <Text style={styles.netWorth}>${user.netWorth}</Text>
+        <Text style={styles.netWorth}>${user.networth}</Text>
       </View>
       <Pressable onPress={signOut} style={styles.button}>
         <Text style={styles.buttonText}>Sign out</Text>
